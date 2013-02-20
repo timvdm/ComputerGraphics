bcf87e3b0db6632a36cc9c68795b9ad2b1fd12b8
timvdm <tim.vandermeersch@gmail.com>
2013-02-20 14:08:09 +0100

    Add plugins for week2's assignments (LineDrawing and 2DLSystems)

---
+++ src/week2/LSystem2D.cc
@@ -0,0 +1,156 @@
+#include "../utils.h"
+#include "../plugin.h"
+
+#include "lines2d.h"
+
+#include <fstream>
+#include <cmath>
+
+#define DEG_TO_RAD M_PI / 180.0
+
+namespace CG {
+
+  class LSystem2D : public Plugin
+  {
+    public:
+      img::EasyImage image(const ini::Configuration &conf)
+      {
+        int size;
+        img::Color bgColor;
+        std::vector<double> lineColor;
+        std::string filename;
+
+        try {
+          size = conf["General"]["size"];
+          bgColor = extractColor(conf["General"]["backgroundcolor"]);
+          lineColor = conf["2DLSystem"]["color"];
+          filename = conf["2DLSystem"]["inputfile"].as_string_or_die();
+        } catch (const std::exception &e) {
+          std::cerr << e.what() << std::endl;
+          return img::EasyImage();
+        }
+
+        // parse L2D file
+        LParser::LSystem2D lSystem;
+        std::ifstream ifs(filename.c_str());
+        assert(ifs);
+        try {
+          ifs >> lSystem;
+        } catch (const std::exception &e) {
+          std::cerr << e.what() << std::endl;
+          return img::EasyImage();
+        }
+        ifs.close();
+
+        // convert LSystem2D to set of lines
+        Lines2D lines;
+        drawLSystem(lSystem, lines);
+
+        // set the line color
+        for (std::size_t i = 0; i < lines.size(); ++i)
+          lines[i].color = Color(lineColor);
+
+
+        return draw_lines(lines, size, bgColor);
+      }
+
+    private:
+      img::Color extractColor(const ini::Entry &entry) const
+      {
+        try {
+          std::vector<double> rgb = entry;
+          return img::Color(rgb.at(0) * 255, rgb.at(1) * 255, rgb.at(2) * 255);
+        } catch (const std::exception &e) {
+          std::cerr << "Could not parse color (using default black)" << std::endl;
+        }
+        
+        return img::Color(0, 0, 0);
+      }
+
+      struct LSystemState
+      {
+        LSystemState(double startingAngle) : angle(startingAngle)
+        {
+        }
+
+        void push()
+        {
+          stack.push_back(std::make_pair(angle, pos));
+        }
+
+        void pop()
+        {
+          angle = stack.back().first;
+          pos = stack.back().second;
+          stack.pop_back();
+        }
+
+        double angle;
+        Point2D pos;
+        std::vector<std::pair<double, Point2D> > stack;
+      };
+
+      void drawLSystem(const LParser::LSystem2D &lSystem, const std::string &commands, Lines2D &lines, int iteration, LSystemState &state)
+      {
+        if (iteration > 0) {
+          // recursive calls...
+          for (std::size_t i = 0; i < commands.size(); ++i)
+            switch (commands[i]) {
+              case '-':
+                state.angle -= DEG_TO_RAD * lSystem.get_angle();
+                break;
+              case '+':
+                state.angle += DEG_TO_RAD * lSystem.get_angle();
+                break;
+              case '(':
+                state.push();
+                break;
+              case ')':
+                state.pop();
+                break;
+              default:
+                drawLSystem(lSystem, lSystem.get_replacement(commands[i]), lines, iteration - 1, state);
+            }
+        } else {
+          // reached deepest recursive level -> draw the lines
+          for (std::size_t i = 0; i < commands.size(); ++i)
+            switch (commands[i]) {
+              case '-':
+                state.angle -= DEG_TO_RAD * lSystem.get_angle();
+                break;
+              case '+':
+                state.angle += DEG_TO_RAD * lSystem.get_angle();
+                break;
+              case '(':
+                state.push();
+                break;
+              case ')':
+                state.pop();
+                break;
+              default:
+                {
+                  // compute new position
+                  Point2D newPos(state.pos.x + std::cos(state.angle), state.pos.y + std::sin(state.angle));
+                  // draw a line?
+                  if (lSystem.draw(commands[i]))
+                    lines.push_back(Line2D(state.pos, newPos));
+                  // move to new position
+                  state.pos = newPos;
+                }
+                break;                
+            }
+        }
+      }
+
+      void drawLSystem(const LParser::LSystem2D &lSystem, Lines2D &lines)
+      {
+        LSystemState state(DEG_TO_RAD * lSystem.get_starting_angle());
+        drawLSystem(lSystem, lSystem.get_initiator(), lines, lSystem.get_nr_iterations(), state);
+      }
+
+
+  };
+
+  PLUGIN_FACTORY("2DLSystem", LSystem2D);
+
+}
---
+++ src/week2/LineDrawing.cc
@@ -0,0 +1,70 @@
+#include "../utils.h"
+#include "../plugin.h"
+
+#include "lines2d.h"
+
+namespace CG {
+
+  class LineDrawing : public Plugin
+  {
+    public:
+      img::Color extractColor(const ini::Entry &entry) const
+      {
+        try {
+          std::vector<double> rgb = entry;
+          return img::Color(rgb.at(0) * 255, rgb.at(1) * 255, rgb.at(2) * 255);
+        } catch (const std::exception &e) {
+          std::cerr << "Could not parse color (using default black)" << std::endl;
+        }
+        
+        return img::Color(0, 0, 0);
+      }
+
+      img::EasyImage image(const ini::Configuration &conf)
+      {
+        std::vector<double> vPoints;
+        std::vector<int> vLineIndexes;
+        img::Color bgColor;
+        std::vector<double> lineColor;
+        int size;
+
+        try {
+          vPoints = conf["LineDrawingProperties"]["points"];
+          vLineIndexes = conf["LineDrawingProperties"]["lineIndexes"];
+          bgColor = extractColor(conf["LineDrawingProperties"]["bgColor"]);
+          lineColor = conf["LineDrawingProperties"]["lineColor"];
+          size = conf["LineDrawingProperties"]["size"];
+        } catch (const std::exception &e) {
+          std::cerr << e.what() << std::endl;
+          return img::EasyImage();
+        }
+
+        // make sure both vectors are a multiple of 2
+        assert((vPoints.size() % 2) == 0);
+        assert((vLineIndexes.size() % 2) == 0);
+
+        // extract points
+        Points2D points;
+        for (std::size_t i = 0; i < vPoints.size(); i += 2)
+          points.push_back(Point2D(vPoints[i], vPoints[i + 1]));
+
+        // make sure the line point indexes are valid
+        assert(*std::min_element(vLineIndexes.begin(), vLineIndexes.end()) >= 0);
+        assert(*std::max_element(vLineIndexes.begin(), vLineIndexes.end()) < points.size());
+
+        // extract lines
+        Lines2D lines;
+        for (std::size_t i = 0; i < vLineIndexes.size(); i += 2)
+          lines.push_back(Line2D(points[vLineIndexes[i]], points[vLineIndexes[i + 1]], lineColor));
+
+        // points copied to lines, not needed anymore
+        points.clear();
+
+        return draw_lines(lines, size, bgColor);
+      }
+
+  };
+
+  PLUGIN_FACTORY("LineDrawing", LineDrawing);
+
+}
