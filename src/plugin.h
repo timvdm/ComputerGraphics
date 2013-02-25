579eb6c12238dc31032a6d8a6253998749349789
timvdm <tim.vandermeersch@gmail.com>
2013-02-26 00:25:52 +0100

    plugin.h: Update docs

--- src/plugin.h
+++ src/plugin.h
@@ -1,5 +1,5 @@
-#ifndef INC_PLUGIN_H
-#define INC_PLUGIN_H
+#ifndef CG_PLUGIN_H
+#define CG_PLUGIN_H
 
 #include "utils.h"
 
@@ -9,17 +9,47 @@ namespace CG {
 
   class Plugin;
 
+  /**
+   * @brief Base class for all plugin factories.
+   *
+   * Each plugin factory implements the type() method which is used to match
+   * the correct plugin with the type field in the General section of the
+   * *.ini files. A plugin factory also implements the instance() method to
+   * return a newly created instance of the correct plugin.
+   *
+   * Inside a plugin implementation file, it is succicient to use the
+   * PLUGIN_FACTORY macro with the correct parameters.
+   */
   class PluginFactory
   {
     public:
       /**
-       * Get the plugin type. This is the 'type' in field in the 'General'
-       * section of the ini file.
+       * @brief Get the plugin type.
+       *
+       * This is the 'type' in field in the 'General' section of the ini file.
+       *
+       * @return The plugin type.
        */
       virtual std::string type() const = 0;
+
+      /**
+       * @brief Get a new instance of the plugin.
+       *
+       * The instance that is returned is a newly created object and should be
+       * deleted when it is not needed anymore.
+       *
+       * @return A pointer to a new instance of the plugin.
+       */
       virtual Plugin* instance() const = 0;
   };
 
+  /**
+   * @brief Macro used to generate code for a plugin factory.
+   *
+   * @param type_str The plugin type as defined in the General section of the
+   *        *.ini files.
+   * @param klass The class name of the plugin class.
+   */
 #define PLUGIN_FACTORY(type_str, klass) \
   class klass##Factory : public PluginFactory \
   { \
@@ -30,20 +60,41 @@ namespace CG {
   }; \
   klass##Factory the##klass##Factory;
 
+  /**
+   * @brief Base class for all plugins to generate images.
+   *
+   * Each derived plugin class implements the image() method to generate the
+   * image for a certain type that is found in the General section of the
+   * *.ini configuration files.
+   */
   class Plugin
   {
     public:
+      /**
+       * @brief Destructor.
+       */
       virtual ~Plugin()
       {
       }
 
       /**
-       * Get the generated image.
+       * @brief Get the generated image.
+       *
+       * Subclasses implement this method to generate the required image for
+       * their type.
+       *
+       * @param conf The ini configuration.
+       *
+       * @return The generated image.
        */
       virtual img::EasyImage image(const ini::Configuration &conf) = 0;
 
       /**
-       * Get a list of all plugins.
+       * @brief Get a list of all plugin factories.
+       *
+       * This method can be used to enumerate all available plugin types.
+       *
+       * @return List of plugin factories.
        */
       static std::list<PluginFactory*>& pluginFactories()
       {
@@ -58,6 +109,16 @@ namespace CG {
         pluginFactories().push_back(factory);
       }
 
+      /**
+       * @brief Find the plugin for the specified type and create an instance
+       * of it.
+       *
+       * @param type The plugin type as defined in the General section of the
+       *        *.ini files.
+       *
+       * @return An instance of the plugin type or 0 if there is no plugin for
+       * the specified type.
+       */
       static Plugin* findPlugin(const std::string &type)
       {
         std::list<PluginFactory*> &list = pluginFactories();
