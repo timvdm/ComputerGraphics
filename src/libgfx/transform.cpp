aa208ec2d0a682595181f19979a59f9da9badf3c
timvdm <tim.vandermeersch@gmail.com>
2013-03-12 18:16:30 +0100

    add orthoMatrix and perspectiveMatrix

--- src/libgfx/transform.cpp
+++ src/libgfx/transform.cpp
@@ -25,17 +25,62 @@ namespace GFX {
     m(1, 2) =   sp;
     m(2, 2) =   cp;
     m(2, 3) = - eye.r;
-    /*
-    m(0, 0) = - st;
-    m(0, 1) = - ct * cp;
-    m(0, 2) =   ct * sp;
-    m(1, 0) =   ct;
-    m(1, 1) = - st * cp;
-    m(1, 2) =   st * sp;
-    m(2, 1) =   sp;
-    m(2, 2) =   cp;
-    m(3, 2) = - eye.r;
-    */
+
+    return m;
+  }
+
+  mat4 orthoMatrix(double l, double r, double t,  double b, double n, double f)
+  {
+    mat4 m(mat4::Identity());
+
+    m(0, 0) = 2.0 / (r - l);
+    m(0, 3) = -(r + l) / (r - l);
+    m(1, 1) = 2.0 / (t - b);
+    m(1, 3) = -(t + b) / (t - b);
+    m(2, 2) = -2.0 / (f - n);
+    m(2, 3) = -(f + n) / (f - n);
+
+    return m;
+  }
+
+  mat4 frustumMatrix(double l, double r, double t, double b, double n, double f)
+  {
+    mat4 m;
+
+    m(0, 0) = 2.0 / (r - l);
+    m(0, 1) = 0.0;
+    m(0, 2) = (r + l) / (r - l);
+    m(0, 3) = 0.0;
+
+    m(1, 0) = 0.0;
+    m(1, 1) = 2.0 / (t - b);
+    m(1, 2) = (t + b) / (t - b);
+    m(1, 3) = 0.0;
+
+    m(2, 0) = 0.0;
+    m(2, 1) = 0.0;
+    m(2, 2) = -(f + n) / (f - n);
+    m(2, 3) = -2.0 * f * n / (f - n);
+
+    m(3, 0) = 0.0;
+    m(3, 1) = 0.0;
+    m(3, 2) = -1.0;
+    m(3, 3) = 0.0;
+
+    return m;
+  }
+
+  mat4 perspectiveMatrix(double fovY, double aspect, double n, double f)
+  {
+    mat4 m(mat4::Zero());
+
+    double c = std::cos(fovY / 2.0) / std::sin(fovY / 2.0);
+
+    m(0, 0) = c / aspect;
+    m(1, 1) = c;
+    m(2, 2) = -(f + n) / (f - n);
+    m(2, 3) = -2.0 * n * f / (f - n);
+    m(3, 2) = -1.0;
 
     return m;
   }
@@ -75,34 +120,7 @@ namespace GFX {
 
     return m;
   }
- 
-  mat4 frustumMatrix(double l, double r, double t, double b, double n, double f)
-  {
-    mat4 m;
 
-    m(0, 0) = 2.0 / (r - l);
-    m(0, 1) = 0.0;
-    m(0, 2) = (r + l) / (r - l);
-    m(0, 3) = 0.0;
-
-    m(1, 0) = 0.0;
-    m(1, 1) = 2.0 / (t - b);
-    m(1, 2) = (t + b) / (t - b);
-    m(1, 3) = 0.0;
-
-    m(2, 0) = 0.0;
-    m(2, 1) = 0.0;
-    m(2, 2) = -(f + n) / (f - n);
-    m(2, 3) = -2.0 * f * n / (f - n);
-
-    m(3, 0) = 0.0;
-    m(3, 1) = 0.0;
-    m(3, 2) = -1.0;
-    m(3, 3) = 0.0;
-
-    return m;
-  }
-  
   mat4 translationMatrix(double x, double y, double z)
   {
     mat4 m(mat4::Identity());
@@ -122,8 +140,8 @@ namespace GFX {
     double s = std::sin(angle);
 
     m(1, 1) =  c;
-    m(1, 2) = -s;
-    m(2, 1) =  s;
+    m(2, 1) = -s;
+    m(1, 2) =  s;
     m(2, 2) =  c;
 
     return m;
@@ -137,8 +155,8 @@ namespace GFX {
     double s = std::sin(angle);
 
     m(0, 0) =  c;
-    m(0, 2) =  s;
-    m(2, 0) = -s;
+    m(2, 0) =  s;
+    m(0, 2) = -s;
     m(2, 2) =  c;
 
     return m;
@@ -152,39 +170,43 @@ namespace GFX {
     double s = std::sin(angle);
 
     m(0, 0) =  c;
-    m(0, 1) = -s;
-    m(1, 0) =  s;
+    m(1, 0) = -s;
+    m(0, 1) =  s;
     m(1, 1) =  c;
 
     return m;
   }
-  
-  /*
+
   mat4 rotationMatrix(double angle, double x, double y, double z)
   {
-    axis.normalise();
+    vec3 axis(x, y, z);
+    axis.normalize();
+
+    x = axis.x();
+    y = axis.y();
+    z = axis.z();
+
     double c = std::cos(angle);
     double s = std::sin(angle);
 
-    double xs = axis.x * s;
-    double ys = axis.y * s;
-    double zs = axis.z * s;
+    double xs = x * s;
+    double ys = y * s;
+    double zs = z * s;
 
-    Matrix m;
+    mat4 m;
 
-    m(1, 1) = axis.x * axis.x * (1.0 - c) + c;
-    m(1, 2) = axis.x * axis.y * (1.0 - c) - zs;
-    m(1, 3) = axis.x * axis.z * (1.0 - c) + ys;
-    m(2, 1) = axis.y * axis.x * (1.0 - c) + zs;
-    m(2, 2) = axis.y * axis.y * (1.0 - c) + c;
-    m(2, 3) = axis.y * axis.z * (1.0 - c) - xs;
-    m(3, 1) = axis.z * axis.x * (1.0 - c) - ys;
-    m(3, 2) = axis.z * axis.y * (1.0 - c) + xs;
-    m(3, 3) = axis.z * axis.z * (1.0 - c) + c;
+    m(0, 0) = x * x * (1.0 - c) + c;
+    m(0, 1) = x * y * (1.0 - c) - zs;
+    m(0, 2) = x * z * (1.0 - c) + ys;
+    m(1, 0) = y * x * (1.0 - c) + zs;
+    m(1, 1) = y * y * (1.0 - c) + c;
+    m(1, 2) = y * z * (1.0 - c) - xs;
+    m(2, 0) = z * x * (1.0 - c) - ys;
+    m(2, 1) = z * y * (1.0 - c) + xs;
+    m(2, 2) = z * z * (1.0 - c) + c;
 
     return m;
   }
-  */
 
   mat4 scaleMatrix(double factor)
   {
@@ -207,6 +229,5 @@ namespace GFX {
 
     return m;
   }
-
 
 }
--- src/libgfx/transform.h
+++ src/libgfx/transform.h
@@ -8,21 +8,102 @@
  */
 namespace GFX {
 
+  //! @name Projection Matrices
+  //@{
+
   /**
-   * @brief Compute the projection matrix given the eye's polar coordinates.
+   * @brief Perspective projection matrix from course material.
    *
-   * @param eye The eye (i.e. camera) polar coordinates.
+   * B. van Houdt, Computer Graphics, University Antwerp, p. 21, code 4123, 2013
+   *
+   * @param x The x coordinate for the eye/camera.
+   * @param y The y coordinate for the eye/camera.
+   * @param z The z coordinate for the eye/camera.
    *
    * @return The projection matrix.
    */
   mat4 projectionMatrix(double x, double y, double z);
 
+  /**
+   * @brief Orthographic projection matrix.
+   *
+   * This projection matrix is the same as the OpenGL glOrtho() matrix.
+   *
+   * @param left Coordinate of the left clipping plane.
+   * @param right Coordinate of the right clipping plane.
+   * @param top Coordinate of the top clipping plane.
+   * @param bottom Coordinate of the bottom clipping plane.
+   * @param near Coordinate of the near clipping plane.
+   * @param far Coordinate of the far clipping plane.
+   *
+   * @return The projection matrix.
+   */
+   mat4 orthoMatrix(double left, double right,
+                    double top,  double bottom,
+                    double near, double far);
 
+  /**
+   * @brief Perspective projection matrix specifying a view frustum.
+   *
+   * This projection matrix is the same as the OpenGL glFrustum() matrix.
+   *
+   * @param left The left side of the view frustum.
+   * @param right The right side of the view frustum.
+   * @param top The top side of the view frustum.
+   * @param bottom The bottom side of the view frustum.
+   * @param near The distance from the camera to the near plane.
+   * @param far The distance from the camera to the far plane.
+   *
+   * @return The projection matrix.
+   */
+   mat4 frustumMatrix(double left, double right,
+                      double top,  double bottom,
+                      double near, double far);
+
+  /**
+   * @brief Perspective projection matrix specifying a view frustum.
+   *
+   * This projection matrix is the same as the OpenGL gluPerspective() matrix.
+   *
+   * @param fovY The field of view in the y direction.
+   * @param aspect The aspect ratio.
+   * @param near Coordinate for the near plane.
+   * @param far Coordinate for the far plane.
+   *
+   * @return The projection matrix.
+   */
+  mat4 perspectiveMatrix(double fovY, double aspect, double near, double far);
+
+  //@}
+
+  //! @name Viewing Matrices
+  //@{
+
+  /**
+   * @brief View matrix from eye to center with up direction.
+   *
+   * This view matrix is the same as the OpenGL gluLookAt() matrix.
+   *
+   * @param eyeX X coordinate for the eye position.
+   * @param eyeY Y coordinate for the eye position.
+   * @param eyeZ Z coordinate for the eye position.
+   * @param centerX X coordinate for the center position.
+   * @param centerY Y coordinate for the center position.
+   * @param centerZ Z coordinate for the center position.
+   * @param upX X coordinate for the up direction.
+   * @param upY Y coordinate for the up direction.
+   * @param upZ Z coordinate for the up direction.
+   *
+   * @return The look at matrix.
+   */
   mat4 lookAtMatrix(double eyeX,    double eyeY,    double eyeZ,
                     double centerX, double centerY, double centerZ,
                     double upX,     double upY,     double upZ);
 
-  mat4 frustumMatrix(double left, double right, double top, double bottom, double near, double far);
+  //@}
+
+  //! @name Modeling Matrices
+  //@{
 
   mat4 translationMatrix(double x, double y, double z);
 
@@ -30,10 +111,12 @@ namespace GFX {
   mat4 yRotationMatrix(double angle);
   mat4 zRotationMatrix(double angle);
 
-  //mat4 rotationMatrix(double angle, double x, double y, double z);
+  mat4 rotationMatrix(double angle, double x, double y, double z);
 
   mat4 scaleMatrix(double factor);
   mat4 scaleMatrix(double x, double y, double z);
+
+  //@}
 
 }
 
